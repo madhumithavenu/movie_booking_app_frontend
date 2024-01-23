55a2316f68474cf876e0a2403659d27c6dc7d4bd
Madhumitha <madhumithavenu03@gmail.com>
2024-01-23 20:55:40 +0530

    Adding function inside api helpers

--- src/api_helpers/api_helpers.jsx
+++ src/api_helpers/api_helpers.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getAllMovies() {
-    const res = await axios.get("http://localhost:5000/movie")
+    const res = await axios.get("/movie")
         .catch((err) => console.log(err));
 
     if (res.status !== 200) {
@@ -13,4 +13,38 @@ export async function getAllMovies() {
         data = await res.data;
     }
     return data;
+};
+
+export async function sendUserAuthRequest(data, signup) {
+    const res = await
+        axios.post(/user/${signup ? "signup" : "login"}, {
+            name: signup ? data.name : "",
+            email: data.email,
+            password: data.password
+        })
+            .catch((err) => console.log(err))
+
+    if (res.status !== 200 && res.status !== 201) {
+        return console.log("Unexpected Error Occured");
+    }
+    const resData = await res.data;
+    return resData;
+
+}
+
+
+export async function sendAdminAuthRequest(data) {
+    const res = await axios
+        .post("/admin/login", {
+            email: data.email,
+            password: data.password,
+        })
+        .catch((err) => console.log(err));
+
+    if (res.status !== 200) {
+        return console.log("Unexpectyed Error");
+    }
+
+    const resData = await res.data;
+    return resData;
 };
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import AuthForm from './AuthForm'
+import AuthForm from './AuthForm';
+import { sendUserAuthRequest } from '../../api_helpers/api_helpers.jsx';
 
 function Auth() {
-  return (
-    <div>
-        <AuthForm/>
-    </div>
-  )
+
+    function getData(data) {
+        console.log("Auth", data);
+        sendUserAuthRequest(data.inputs, data.signup)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <AuthForm onSubmit={getData} isAdmin={false} />
+
+    )
 }
 
-export default Auth
+export default Auth;
