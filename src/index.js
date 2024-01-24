164428c0aad151a7e713235e55b7120ebcd97c04
Madhumitha <madhumithavenu03@gmail.com>
2024-01-24 15:02:10 +0530

    creating user profile

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { store } from './store';
+import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL = "http://localhost:5000";
