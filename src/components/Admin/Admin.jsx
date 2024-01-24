7b2c159261f48ede42e37f794416fbab9b6e6a8d
Madhumitha <madhumithavenu03@gmail.com>
2024-01-24 13:26:03 +0530

    Adding functions inside auth and admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import AuthForm from '../Auth/AuthForm.jsx'
+import { useDispatch } from 'react-redux';
+import { adminActions } from '../../store/index.js';
+import { sendAdminAuthRequest } from '../../api_helpers/api_helpers.jsx';
 
 function Admin() {
+    const dispatch = useDispatch();
+
+    function onResReceived(data) {
+        console.log(data);
+        dispatch(adminActions.login())
+        localStorage.setItem("AdminID", data.id);
+        localStorage.setItem("Token", data.token);
+    }
+
     function getData(data) {
         console.log("Admin", data);
+        sendAdminAuthRequest(data.inputs, data.signup)
+            .then(onResReceived)
+            .catch((err) => console.log(err));
     };
     return (
         <div>
@@ -11,5 +26,3 @@ function Admin() {
         </div>
     )
 }
-
-export default Admin;
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import AuthForm from './AuthForm';
 import { sendUserAuthRequest } from '../../api_helpers/api_helpers.jsx';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../../store/index.js';
 
 function Auth() {
+    const dispatch = useDispatch();
+
+    function onResReceived(data) {
+        console.log(data);
+        dispatch(userActions.login())
+        localStorage.setItem("userID", data.id)
+
+    }
 
     function getData(data) {
         console.log("Auth", data);
         sendUserAuthRequest(data.inputs, data.signup)
-            .then((res) => console.log(res))
+            .then(onResReceived)
             .catch((err) => console.log(err));
     }
 
